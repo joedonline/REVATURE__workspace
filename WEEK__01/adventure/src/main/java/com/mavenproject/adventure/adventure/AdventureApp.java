efb9050c5e0ab8b53463c441fb59a1e19ded59e5
Joe Domingo <contact@joedomingo.online>
2019-12-16 16:15:20 -0500

    NOTES update.

--- WEEK__01/adventure/src/main/java/com/mavenproject/adventure/adventure/AdventureApp.java
+++ WEEK__01/adventure/src/main/java/com/mavenproject/adventure/adventure/AdventureApp.java
@@ -5,18 +5,29 @@ import java.util.Scanner;
 public class AdventureApp {
 
 	private static Scanner sc = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 		Boolean hasGivenValidInput = false;
 		
-		while (!hasGivenValidInput) {
-			System.out.println("Enter the text stringexample: ");
-			String userString = sc.next();
-			if (userString .equalsIgnoreCase("stringexample")) {
-				System.out.println("It works!!");
+		while(!hasGivenValidInput) {
+			
+			System.out.println("You find yourself in a brightly lit hallway.  There are doors to the NORTH and the SOUTH.");
+			String userString = sc.nextLine();
+			
+			if (userString.equalsIgnoreCase("south")) {
+				System.out.println("This room is under construction.  Your adventure is over.");
+				hasGivenValidInput = true;
+			} else if (userString.equalsIgnoreCase("north")) {
+				System.out.println("This room is full of spiders." +
+						"Do you want to leave the adventure?");
+				String leaveAdventureConfirmation = sc.nextLine();
+				while(!leaveAdventureConfirmation.equalsIgnoreCase("yes")) {
+					System.out.println("There are really a lot of spiders, are you sure?");
+					leaveAdventureConfirmation = sc.nextLine();
+				}
 				hasGivenValidInput = true;
 			} else {
-				System.out.println("It didn't work.");
+				System.out.println("Invalid input");
 			}
 		}
 	}
