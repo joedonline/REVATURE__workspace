1f9e724a9408f837d2960b83233d857f49b46bc2
Joe Domingo <contact@joedomingo.online>
2020-01-29 15:53:38 -0500

    NOTES, WEEK 7, WED update..

--- WEEK__07/workspace/books/src/main/java/com/revature/books/controllers/HelloWorldController.java
+++ WEEK__07/workspace/books/src/main/java/com/revature/books/controllers/HelloWorldController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -62,6 +64,15 @@ public class HelloWorldController {
 	@GetMapping("/pathvar/{myvar}")
 	public String echo(@PathVariable String myvar) {
 		return "You var: " + myvar;
+	}
+	
+	@PostMapping("/addOneToGenre")
+	@ResponseBody
+	public Genre addOneToRating(@RequestBody Genre genre) {
+		
+		genre.setRating(genre.getRating() + 1);
+		return genre;
+		
 	}
 
 }
